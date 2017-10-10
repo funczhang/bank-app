0e528f1a08d3c78e371e7f41be886dffe2e9b54c
NickZhang <funczhang@users.noreply.github.com>
2017-10-10 10:43:05 +0800

    添加建议反馈页面

--- src/pages/common/tabbar.vue
+++ src/pages/common/tabbar.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   tabbar(v-model="toTab")
-    tabbar-item(on-item-click="toPage(0)" to="/")
+    tabbar-item(on-item-click="toPage(0)" link="/")
       img(slot="icon" src="../../assets/imgs/icon-home.png")
       img(slot="icon-active" src="../../assets/imgs/icon-home-active.png")
       span(slot="label") 首页
-    tabbar-item(on-item-click="toPage(1)" to="/loan")
+    tabbar-item(on-item-click="toPage(1)" link="/loan")
       img(slot="icon" src="../../assets/imgs/icon-loan2.png")
       img(slot="icon-active" src="../../assets/imgs/icon-loan-active.png")
       span(slot="label") 贷款
-    tabbar-item(on-item-click="toPage(2)" to="/my")
+    tabbar-item(on-item-click="toPage(2)" link="/my")
       img(slot="icon" src="../../assets/imgs/icon-my.png")
       img(slot="icon-active" src="../../assets/imgs/icon-my-active.png")
       span(slot="label") 我的
-    tabbar-item(on-item-click="toPage(3)" to="/more")
+    tabbar-item(on-item-click="toPage(3)" link="/more")
       img(slot="icon" src="../../assets/imgs/icon-more.png")
       img(slot="icon-active" src="../../assets/imgs/icon-more-active.png")
       span(slot="label") 更多

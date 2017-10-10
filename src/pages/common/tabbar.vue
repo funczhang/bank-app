6026e53ea6247b36c3ca895504eaf79f49a5f179
NickZhang <funczhang@users.noreply.github.com>
2017-10-10 09:37:30 +0800

    添加tab切换

--- src/pages/common/tabbar.vue
+++ src/pages/common/tabbar.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   tabbar(v-model="toTab")
-    tabbar-item(on-item-click="toPage(0)")
+    tabbar-item(on-item-click="toPage(0)" to="/")
       img(slot="icon" src="../../assets/imgs/icon-home.png")
       img(slot="icon-active" src="../../assets/imgs/icon-home-active.png")
       span(slot="label") 首页
-    tabbar-item(on-item-click="toPage(1)")
+    tabbar-item(on-item-click="toPage(1)" to="/loan")
       img(slot="icon" src="../../assets/imgs/icon-loan2.png")
       img(slot="icon-active" src="../../assets/imgs/icon-loan-active.png")
       span(slot="label") 贷款
-    tabbar-item(on-item-click="toPage(2)")
+    tabbar-item(on-item-click="toPage(2)" to="/my")
       img(slot="icon" src="../../assets/imgs/icon-my.png")
       img(slot="icon-active" src="../../assets/imgs/icon-my-active.png")
       span(slot="label") 我的
-    tabbar-item(on-item-click="toPage(3)")
+    tabbar-item(on-item-click="toPage(3)" to="/more")
       img(slot="icon" src="../../assets/imgs/icon-more.png")
       img(slot="icon-active" src="../../assets/imgs/icon-more-active.png")
       span(slot="label") 更多

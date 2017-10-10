1f4b2b56e78a2d2958dfaaa59dc2843adc73b8d4
NickZhang <funczhang@users.noreply.github.com>
2017-10-10 09:12:06 +0800

    tab切换

--- src/pages/common/tabbar.vue
+++ src/pages/common/tabbar.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   tabbar(v-model="toTab")
-    tabbar-item(on-item-click="toPage(0)")
+    tabbar-item(on-item-click="toPage(0)" link="/")
       img(slot="icon" src="../../assets/imgs/icon-home.png")
       img(slot="icon-active" src="../../assets/imgs/icon-home-active.png")
       span(slot="label") 首页
-    tabbar-item(on-item-click="toPage(1)")
+    tabbar-item(on-item-click="toPage(1)" link="/loan")
       img(slot="icon" src="../../assets/imgs/icon-loan2.png")
       img(slot="icon-active" src="../../assets/imgs/icon-loan-active.png")
       span(slot="label") 贷款
-    tabbar-item(on-item-click="toPage(2)")
+    tabbar-item(on-item-click="toPage(2)" link="/my")
       img(slot="icon" src="../../assets/imgs/icon-my.png")
       img(slot="icon-active" src="../../assets/imgs/icon-my-active.png")
       span(slot="label") 我的
-    tabbar-item(on-item-click="toPage(3)")
+    tabbar-item(on-item-click="toPage(3)" link="/more")
       img(slot="icon" src="../../assets/imgs/icon-more.png")
       img(slot="icon-active" src="../../assets/imgs/icon-more-active.png")
       span(slot="label") 更多
@@ -20,7 +20,6 @@
 
 <script>
 import { Tabbar, TabbarItem } from 'vux'
-
 export default {
   components: {
     Tabbar,
@@ -50,7 +49,6 @@ export default {
       console.log('1111')
     },
     toPage (flag) {
-      debugger
       if (flag === 0) {
         this.$router.push('/')
       } else if (flag === 1) {
